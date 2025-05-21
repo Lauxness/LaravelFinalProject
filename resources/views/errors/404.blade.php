c6e5de4e1a8f79b90bbf36e00bc41992be27c4e8
Laux <2201104208@student.buksu.edu.ph>
2025-05-22 07:08:30 +0800

    asdhfjahsdf

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 <!-- [Head] start -->
 
 <head>
-    <title>404 | Mantis Bootstrap 5 Admin Template</title>
+    <title>Not Found</title>
     <!-- [Meta] -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
